923e0d60d8389b438bd2bac72eece9cc7f5b869c
Roosa K <rakkyl@utu.fi>
2018-04-11 22:09:19 +0300

    Adds scenes that are needed for the user to reserve a ticket.
    MainController now has a basic method for changing the scene.

--- Museojunat/src/varaus/MainController.java
+++ Museojunat/src/varaus/MainController.java
@@ -10,35 +10,33 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainController {
-	
-	public void sayHi() {
-		System.out.println("Hi!");
-	}
 
-	/**
-	 * When this method is called,  it will change the scene
-	 * @throws IOException 
-	 */
 	
-	public void changeScene(ActionEvent event) throws IOException {
-		Parent tableViewParent = FXMLLoader.load(getClass().getResource("view/UserView00.fxml"));
-		Scene tableViewScene = new Scene(tableViewParent);
-		
-		//This line gets the Stage information
-		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-		
-		window.setScene(tableViewScene);
-		window.show();
+	// --------- Methods for changing the scene, when a button is clicked ---------
+	
+	//naming convention: changeTo[wanted scene name], for example: changeToLogin
+	
+	public void changeToUserView(ActionEvent event) throws IOException {
+		changeScene(event, "view/UserView00.fxml");
 	}
 	
-	public void goToLogin(ActionEvent event) throws IOException {
-		Parent logInView = FXMLLoader.load(getClass().getResource("view/Ruutu00.fxml"));
-		Scene logInScene = new Scene(logInView);
+	public void changeToLogin(ActionEvent event) throws IOException {
+		changeScene(event, "view/Ruutu00.fxml");
+	}
+	
+	
+	// A general code for changing the scene in the window. Parameters are the ActionEvent and the
+	//name of the scene that you want to display.
+	public void changeScene(ActionEvent event, String sceneName) throws IOException {
+		
+		Parent wantedScene = FXMLLoader.load(getClass().getResource(sceneName));
+		Scene newScene = new Scene (wantedScene);
 		
 		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
 		
-		window.setScene(logInScene);
+		window.setScene(newScene);
 		window.show();
+		
 	}
 	
 }
